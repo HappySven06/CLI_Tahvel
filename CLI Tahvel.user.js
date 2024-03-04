ae8a4107a7b2b976c1806933cab607b0b6f7869e
Sven <Sven.Laht@gmail.com>
2024-03-04 21:13:33 +0200

    Base files

---
+++ CLI Tahvel.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         CLI Tahvel
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-04
+// @description  CLI for Tahvel
+// @author       Sven Laht
+// @match        https://tahvel.edu.ee/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=edu.ee
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
