4ee95f67cc3a2c0107c3a4751b6ba68a84b71bf7
Sven <Sven.Laht@gmail.com>
2024-03-04 23:36:43 +0200

    Fixed stileing

--- CLI Tahvel.user.js
+++ CLI Tahvel.user.js
@@ -12,10 +12,10 @@
 
 /* global $ */
 
-(function() {
-    'use strict';
+(function () {
+	'use strict';
 
-    var CSS = `
+	var CSS = `
         :root {
             --color: #aaa;
             --background: #000;
@@ -25,59 +25,56 @@
         }
     `;
 
-    function removeElements() {
-        var head = document.head;
-        var body = document.body;
+	function removeElements() {
+		var head = document.head;
+		var body = document.body;
 
-        // Remove all elements in head
-        while (head.firstChild) {
-            head.removeChild(head.firstChild);
-        }
+		while (head.firstChild) {
+			head.removeChild(head.firstChild);
+		};
 
-        // Remove all elements in body except html, head, and body
-        var allElements = body.children;
-        for (var i = allElements.length - 1; i >= 0; i--) {
-            var tagName = allElements[i].tagName.toLowerCase();
-            if (tagName !== "html" && tagName !== "head" && tagName !== "body") {
-                body.removeChild(allElements[i]);
-            } else {
-                allElements[i].innerHTML = ''; // Empty the content of html, head, and body tags
-            }
-        }
-    }
+		var allElements = body.children;
+		for (var i = allElements.length - 1; i >= 0; i--) {
+			var tagName = allElements[i].tagName.toLowerCase();
+			if (tagName !== "html" && tagName !== "head" && tagName !== "body") {
+				body.removeChild(allElements[i]);
+			}
+			else {
+				allElements[i].innerHTML = '';
+			}
+		};
+	};
 
-    function loadDependencies(callback) {
-        var jqueryScript = document.createElement('script');
-        jqueryScript.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-        jqueryScript.onload = function() {
-            var terminalScript = document.createElement('script');
-            terminalScript.src = 'https://cdn.jsdelivr.net/npm/jquery.terminal@2.x.x/js/jquery.terminal.min.js';
-            terminalScript.onload = function() {
-                // Now that jQuery and jQuery Terminal are loaded,
-                // let's load the CSS file for jQuery Terminal.
-                var terminalCSS = document.createElement('link');
-                terminalCSS.rel = 'stylesheet';
-                terminalCSS.href = 'https://cdn.jsdelivr.net/npm/jquery.terminal@2.x.x/css/jquery.terminal.min.css';
-                terminalCSS.onload = callback; // Call the callback once CSS is loaded
-                document.head.appendChild(terminalCSS);
-            };
-            document.head.appendChild(terminalScript);
-        };
-        document.head.appendChild(jqueryScript);
-    }
+	function loadDependencies(callback) {
+		var jqueryScript = document.createElement('script');
+		jqueryScript.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+		jqueryScript.onload = function () {
+			var terminalScript = document.createElement('script');
+			terminalScript.src = 'https://cdn.jsdelivr.net/npm/jquery.terminal@2.x.x/js/jquery.terminal.min.js';
+			terminalScript.onload = function () {
+				var terminalCSS = document.createElement('link');
+				terminalCSS.rel = 'stylesheet';
+				terminalCSS.href = 'https://cdn.jsdelivr.net/npm/jquery.terminal@2.x.x/css/jquery.terminal.min.css';
+				terminalCSS.onload = callback;
+				document.head.appendChild(terminalCSS);
+			};
+			document.head.appendChild(terminalScript);
+		};
+		document.head.appendChild(jqueryScript);
+	};
 
-    function initTerminal() {
-        $('body').terminal({
-            hello: function(what) {
-                this.echo('Hello, ' + what + '. Welcome to this terminal.');
-            }
-        }, {
-            greetings: 'My First Web Terminal'
-        });
-    }
+	function initTerminal() {
+		$('body').terminal({
+			hello: function (what) {
+				this.echo('Hello, ' + what + '. Welcome to this terminal.');
+			}
+		}, {
+			greetings: 'My First Web Terminal'
+		});
+	}
 
-    removeElements();
+	removeElements();
 
-    loadDependencies(initTerminal);
-    GM_addStyle(CSS);
+	loadDependencies(initTerminal);
+	GM_addStyle(CSS);
 })();
